3ad79168c42f6292c72d1da76b9eee4f68e2f5ef
Daniel A. Battaglia <dab0623@navitus.com>
2021-01-15 12:17:37 -0600

    updated README.md

--- Program.cs
+++ Program.cs
@@ -2,34 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 
-/*
- * https://www.cms.gov/Regulations-and-Guidance/Administrative-Simplification/NationalProvIdentStand/Downloads/NPIcheckdigit.pdf
- * Luhn Formula for Modulus 10 “double-add-double” Check Digit
- * The Luhn check digit formula is calculated as follows: 
- * 1.Double the value of alternate digits beginning with the rightmost digit.
- * 2.Add the individual digits of the products resulting from step 1 to the unaffected digits from the original number.
- * 3.Subtract the total obtained in step 2 from the next higher number ending in zero.  This is the check digit.
- *   If the total obtained in step 2 is a number ending in zero, the check digit is zero.
- * 
- * Example of Check Digit Calculation for NPI used without Prefix
- * Assume the 9-position identifier part of the NPI is 123456789.  
- * Using the Luhn formula on the identifier portion, the check digit is calculated as follows:
- * NPI without check digit:
- * 1     2     3     4     5     6     7     8     9 
- * 
- * Step 1: Double the value of alternate digits, beginning with the rightmost digit.
- * 2            6          10          14          18 
- * 
- * Step 2:  Add constant 24, to account for the 80840 prefix that would be present on a card issuer identifier, 
- * plus the individual digits of products of doubling, plus unaffected digits.
- * 24 + 2 + 2 + 6 + 4 + 1 + 0 + 6 + 1 + 4 + 8 + 1 + 8 = 67 
- * 
- * Step 3:  Subtract from next higher number ending in zero. 
- * 70 – 67 = 3 
- * Check digit = 3 
- * NPI with check digit = 1234567893 
-*/
-
 namespace NPIValidator
 {
     class Program
